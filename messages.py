f8273194459f0cf17f0c2bbcd5bb5209dd6b23c0
Nicole <nicole.pyzza@gmail.com>
2022-09-09 13:34:15 -0400

    adding to requirements

--- messages.py
+++ messages.py
@@ -1,11 +1,11 @@
 random_phrases = [
-    f"Hi, MEMBER! I've alerted someone that you're in the waiting room. Hang tight, buddy.",
-    "message2, test MEMBER",
-    "testmessage",
-    "message4532423"
+    "Hi, MEMBER! I've alerted someone that you're in the waiting room. Hang tight, buddy.",
+    "J.A.R.V.I.S here! I'll let someone know you're waiting.",
+    "Welcome, MEMBER! I'll let someone know you're in the waiting room.",
+    "Hi there, MEMBER. One second while I let the admins know you're waiting."
 ]
 
 woops = [
     "You are not authorized to access this area.",
-    "sry not sry"
+    "Sorry, MEMBER. No one's online to let you in... :("
 ]
